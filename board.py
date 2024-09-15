cd95cecf121b85a5bd831495a4ec627dd4cd512a
Ethan Doughty <130803522+EthanDoughty@users.noreply.github.com>
2024-09-14 19:56:54 -0500

    Changed names to board1 and main

--- board.py
+++ board.py
@@ -1,22 +1,61 @@
-#TODO create a way to enter ships for each board. Grid coordinates should be marked. Error checking 
-#needs to be implemented for ship placement(Can't place a ship on top of another and a ship can't cross the game border)
 class Board:
     def __init__(self, numShips):
         self.size = 10
         self.numShips = numShips
-        self.col = []
-        self.row = []
-    #Build an empty board based on the size of the board
-    def buildBoard(self):
-        #Consider changing to a nested list with grid=[rows[cols]]
+        self.grid = list()
         for i in range(self.size):
-            self.row.append(0)
-            self.col.append(0)
+            self.grid.append(list())
+
+    def buildBoard(self):
+        for row in self.grid:
+            for i in range(self.size):
+                row.append(0)
+    
+    def ship_placement(self):
+        ship_num = 1
+        for i in range(self.numShips):
+            print("****Ship #", ship_num, " ( 1 x", ship_num, ")****")
+            print("\n")
+            if ship_num >= 2:
+                print("Must enter adjacent tiles for each ship. Do not repeat tiles.")
+            row_num = int(input("Enter row number for ship:"))
+            column_letter = input("Enter a column letter:")
+            print("\n")
+            column_letter = column_letter.upper()
+
+
+            if column_letter == 'A':
+                column_letter = 1
+            elif column_letter == 'B':
+                column_letter = 2
+            elif column_letter == 'C':
+                column_letter = 3
+            elif column_letter == 'D':
+                column_letter = 4
+            elif column_letter == 'E':
+                column_letter = 5
+            elif column_letter == 'F':
+                column_letter = 6
+            elif column_letter == 'G':
+                column_letter = 7
+            elif column_letter == 'H':
+                column_letter = 8
+            elif column_letter == 'I':
+                column_letter = 9
+            elif column_letter == 'J':
+                column_letter = 10
+
+
+            row_num -= 1
+            column_letter -= 1
+            self.grid[row_num][column_letter] = 1
+            ship_num += 1
+
+
 
     def showBoard(self):
-        #TODO Create a prettier way of displaying the game board. 
-        #Perhaps include a grid pattern or a small window application using pygame
-        for i in range(len(self.row)):
-            print("\n")
-            for j in range(len(self.col)):
-                print("0", end=' ')
+        print("\n")
+        print("****Board****")
+        for row in self.grid:
+            print(row)
+
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from board import Board
 let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
 
 def main():
-    #TODO Create a main game loop. Iclude a victory condition (eg. All ships are sunk or a user decides to quit)
     numShips = int(input("Enter the number of ships per player: "))
     board1 = Board(numShips)
     board2 = Board(numShips)
     board1.buildBoard()
     board2.buildBoard()
     board1.showBoard()
+    print("\n")
+    board1.ship_placement()
+    board2.ship_placement()
 
 
 if __name__ == "__main__":
