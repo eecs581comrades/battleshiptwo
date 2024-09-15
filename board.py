44f2e755d838215dd5ae8abd5c217b1cfbe377c1
amurphy2001 <murphyheat@yahoo.com>
2024-09-15 14:17:52 -0500

    Add files via upload
    Extensive updates to the main function including gameplay setup. Additional changes to board to support main and implement gameplay.

--- board.py
+++ board.py
@@ -1,61 +1,78 @@
 class Board:
-    def __init__(self, numShips):
+    def __init__(self, numShips): #init changes to the way the grid is setup and the hits to calculate endgame.
         self.size = 10
         self.numShips = numShips
-        self.grid = list()
-        for i in range(self.size):
-            self.grid.append(list())
+        self.grid = [['.' for _ in range(self.size)] for _ in range(self.size)]
+        self.hits = 0
 
-    def buildBoard(self):
-        for row in self.grid:
-            for i in range(self.size):
-                row.append(0)
+    def buildBoard(self): #Not needed anymore
+        pass
     
-    def ship_placement(self):
+    def ship_placement(self, let_to_num): #New setup for ship placement to use horizontal or vertical. Also incorporates what was previously in buildBoard. Tried to make output look cleaner.
         ship_num = 1
         for i in range(self.numShips):
-            print("****Ship #", ship_num, " ( 1 x", ship_num, ")****")
-            print("\n")
-            if ship_num >= 2:
-                print("Must enter adjacent tiles for each ship. Do not repeat tiles.")
-            row_num = int(input("Enter row number for ship:"))
-            column_letter = input("Enter a column letter:")
-            print("\n")
-            column_letter = column_letter.upper()
+            print(f"**** Ship #{ship_num} (1 x {ship_num}) ****")
+            while True:
+                try:
+                    row_num = int(input("Enter row number (1-10): ")) - 1 #Row and column
+                    column_letter = input("Enter column letter (A-J): ").upper()
+                    column_number = let_to_num.get(column_letter, -1)
+                    if column_number == -1 or row_num < 0 or row_num >= self.size: #Validity checks
+                        print("Invalid row/column. Try again.")
+                        continue 
+                    orientation = input("Enter orientation (H for Horizontal, V for Vertical): ").upper() #Direction
+                    
+                    if orientation == 'H': #Ship setup checks
+                        if column_number + ship_num > self.size:
+                            print("Ship does not fit horizontally. Try again.")
+                            continue
+                        if any(self.grid[row_num][column_number + j] != '.' for j in range(ship_num)):
+                            print("Ships cannot overlap. Try again.")
+                            continue
 
+                        for j in range(ship_num): #Placement
+                            self.grid[row_num][column_number + j] = 'S'
 
-            if column_letter == 'A':
-                column_letter = 1
-            elif column_letter == 'B':
-                column_letter = 2
-            elif column_letter == 'C':
-                column_letter = 3
-            elif column_letter == 'D':
-                column_letter = 4
-            elif column_letter == 'E':
-                column_letter = 5
-            elif column_letter == 'F':
-                column_letter = 6
-            elif column_letter == 'G':
-                column_letter = 7
-            elif column_letter == 'H':
-                column_letter = 8
-            elif column_letter == 'I':
-                column_letter = 9
-            elif column_letter == 'J':
-                column_letter = 10
+                    elif orientation == 'V': #Ship setup checks
+                        if row_num + ship_num > self.size:
+                            print("Ship does not fit vertically. Try again.")
+                            continue
+                        if any(self.grid[row_num + j][column_number] != '.' for j in range(ship_num)):
+                            print("Ships cannot overlap. Try again.")
+                            continue
 
+                        for j in range(ship_num): #Placement
+                            self.grid[row_num + j][column_number] = 'S'
+                    else:
+                        print("Invalid orientation. Please enter H or V.")
+                        continue
+                    
+                    break
 
-            row_num -= 1
-            column_letter -= 1
-            self.grid[row_num][column_letter] = 1
+                except ValueError:
+                    print("Invalid input. Please try again.")
+            
+            self.showBoard()  #Show the new board
             ship_num += 1
 
+    def showBoard(self): #Small showBoard updates to make it look nicer
+        print("\n**** Board ****")
+        print("  A B C D E F G H I J")
+        for idx, row in enumerate(self.grid):
+            display_row = [str(cell) for cell in row]
+            print(f"{idx + 1:2} " + " ".join(display_row))
 
+    def take_shot(self, row, col): #Updates the board for hits and misses
+        if self.grid[row][col] == 'S': #Ships
+            print("Hit!")
+            self.grid[row][col] = 'H' #Hits
+            self.hits += 1
+        elif self.grid[row][col] == '.': #Empty
+            print("Miss!")
+            self.grid[row][col] = 'M'  #Misses
+        else:
+            print("Already targeted this spot. Try again.")
 
-    def showBoard(self):
-        print("\n")
-        print("****Board****")
-        for row in self.grid:
-            print(row)
-
+    def has_lost(self): #Endgame check
+        total_ship_cells = sum(range(1, self.numShips + 1))
+        return self.hits == total_ship_cells
--- main.py
+++ main.py
@@ -1,20 +1,66 @@
 from board import Board
 
 #Dictionary can be used as a global var to map chars to ints
-let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
 
+def get_shot(): #Function to get shots from each player
+    while True:
+        try:
+            row = int(input("Enter row number (1-10): ")) - 1
+            col_letter = input("Enter column letter (A-J): ").upper()
+            col = let_to_num.get(col_letter, -1)
+
+            if col == -1 or row < 0 or row >= 10:
+                print("Invalid row/column. Try again.")
+                continue
+
+            return row, col
+        except ValueError:
+            print("Invalid input. Please try again.")
 def main():
-    numShips = int(input("Enter the number of ships per player: "))
+
+    numShips = int(input("Enter the number of ships per player (1 to 5): "))
+
+    while numShips < 1 or numShips > 5: #New check to make sure the number is valid
+        print("Invalid number of ships! Please choose between 1 and 5.")
+        numShips = int(input("Enter the number of ships per player (1 to 5): "))
     board1 = Board(numShips)
     board2 = Board(numShips)
-    board1.buildBoard()
-    board2.buildBoard()
-    board1.showBoard()
-    print("\n")
-    board1.ship_placement()
-    board2.ship_placement()
 
+    print("Player 1's Board:")
+    board1.showBoard()
+    print("\nPlayer 2's Board:")
+    board2.showBoard()
+    
+    print("\nPlayer 1, place your ships:")
+    board1.ship_placement(let_to_num) #New function to make setting up the boards easier
+    print("\nPlayer 2, place your ships:")
+    board2.ship_placement(let_to_num)
+
+    player_turn = 1 #Gameplay setup in main. Changes to get target from each player then outputs the board they shot at. Endgame check if all spots are hit.
+    while True:
+        if player_turn == 1:
+            print("\nPlayer 1, take your shot!")
+            row, col = get_shot()
+            board2.take_shot(row, col)
+            board2.showBoard()
+
+            if board2.has_lost():
+                print("Player 1 wins!")
+                break
+
+            player_turn = 2
+        else:
+            print("\nPlayer 2, take your shot!")
+            row, col = get_shot()
+            board1.take_shot(row, col)
+            board1.showBoard()
+
+            if board1.has_lost():
+                print("Player 2 wins!")
+                break
+
+            player_turn = 1
 
 if __name__ == "__main__":
     main()
-
