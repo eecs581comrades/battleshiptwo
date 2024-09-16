b2718651b2a443d2dac464da93ebef1adbb8856d
amurphy2001 <murphyheat@yahoo.com>
2024-09-15 20:28:09 -0500

    Update main.py
    Commenting and documentation!

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-from board import Board
+#Battleship Main File
+#This file contains the main functions and gameplay mechanics for the game battleship. This initializes the game and runs all interactions between players.
+#No inputs or outputs
+#Authors: Aiden Murphy, Jack Doughty, Jack Pigott, Vy Luu, Daniel Bobadilla
+#Supplemental help from ChatGPT and StackOverflow
+#Creation Date: 09-13-2024
+#Newest Commit: 09-15-2024
 
+
+from board import Board
 #Dictionary can be used as a global var to map chars to ints
 let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
 
-def get_shot(): #Function to get shots from each player
+#Get_Shot gets the coordinates from each player as a target for their shot
+def get_shot():
     while True:
         try:
+            #Gets user input for each required field
             row = int(input("Enter row number (1-10): ")) - 1
             col_letter = input("Enter column letter (A-J): ").upper()
             col = let_to_num.get(col_letter, -1)
 
+            #Error checking for validity
             if col == -1 or row < 0 or row >= 10:
                 print("Invalid row/column. Try again.")
                 continue
 
             return row, col
+        #Error checking for validity
         except ValueError:
             print("Invalid input. Please try again.")
+
+#Main function of our program, handles the game setup, as well as the gameplay between each user
 def main():
 
+    #Sets up and initalizes game, creates board for each user by calling ship_placement
     numShips = int(input("Enter the number of ships per player (1 to 5): "))
-
-    while numShips < 1 or numShips > 5: #New check to make sure the number is valid
+    while numShips < 1 or numShips > 5: #Error checking
         print("Invalid number of ships! Please choose between 1 and 5.")
         numShips = int(input("Enter the number of ships per player (1 to 5): "))
     board1 = Board(numShips)
     board2 = Board(numShips)
-
     print("Player 1's Board:")
     board1.showBoard()
     print("\nPlayer 2's Board:")
     board2.showBoard()
-    
     print("\nPlayer 1, place your ships:")
     board1.ship_placement(let_to_num) #New function to make setting up the boards easier
     print("\nPlayer 2, place your ships:")
@@ -45,6 +57,7 @@ def main():
             board2.take_shot(row, col)
             board2.showBoard()
 
+            #Endgame check
             if board2.has_lost():
                 print("Player 1 wins!")
                 break
@@ -56,6 +69,7 @@ def main():
             board1.take_shot(row, col)
             board1.showBoard()
 
+            #Endgame check
             if board1.has_lost():
                 print("Player 2 wins!")
                 break
